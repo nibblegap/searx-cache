0dde07cc211a04936486f0826634030e6ff2d323
Nicolas Gelot <nicofonk@gmail.com>
2019-01-07 23:44:05 +0100

    Replace cgi.escape by html.escape
    This patch removes the deprecation warning, "cgi.escape is deprecated,
    use html.escape instead".

--- searx/webapp.py
+++ searx/webapp.py
@@ -46,7 +46,7 @@ except:
     from sys import exit
 
     exit(1)
-from cgi import escape
+from html import escape
 from datetime import datetime, timedelta
 from werkzeug.contrib.fixers import ProxyFix
 from flask import (
